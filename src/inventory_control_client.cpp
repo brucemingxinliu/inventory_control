66b67a03d9aa9f490709aa69d75f37f1bcb6bd3c
brucemingxinliu <mxl592@case.edu>
2017-04-19 00:23:31 -0400

    Update inventory_control_client.cpp

--- src/inventory_control_client.cpp
+++ src/inventory_control_client.cpp
@@ -2,9 +2,9 @@
 //create by Bruce Mingxin Liu
 
 #include <ros/ros.h>
-#include "BinManger.h"
+//#include "BinManger.h"
 #include <cwru_ariac/Part.h>
-#include <inventory_control_service/InventoryServiceMsg.h>
+#include <cwru_ariac/InventoryServiceMsg.h>
 #include "CameraEstimator.h"
 #include <iostream>
 #include <string>
@@ -13,23 +13,26 @@ using namespace std;
 
 int main(int argc, char **argv){
 
-	ros::int(argc, argv, "inventory_control_client");
+	ros::init(argc, argv, "inventory_control_client");
 	ros::NodeHandle n;
-	ros::ServiceClient client = n.serviceClient<inventory_control_service::InventoryServiceMsg>("look_up_parts_space");
-	inventory_control_service::InventoryServiceMsg srv;
-	String part_name;
+	ros::ServiceClient client = n.serviceClient<cwru_ariac::InventoryServiceMsg>("look_up_parts_space");
+	cwru_ariac::InventoryServiceMsg srv;
+	string part_name;
 
 	while (ros::ok()){
+            for(geometry_msgs::PoseStamped pose : srv.response.poses)
+            {
 		cout<<endl;
 		cout<<"What's the part's name that has the highest priority? ";
 		cin>>part_name;
 		srv.request.part_name = part_name;// the name of the part is sending to the service to check spacial avaliablity
 		if(client.call(srv)){
-			cout << "the x coordinate is " << srv.response.location_x << "and the y coordinate is" << srv.response.location_y << endl;
+			cout << "the x coordinate is " << pose.pose.position.x << "and the y coordinate is" << pose.pose.position.y << endl;
 
 
 
 		}
+             }
 	}
 
 
