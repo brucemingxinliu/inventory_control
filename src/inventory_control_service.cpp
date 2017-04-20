e2518923c011608d7e366c44add4ad568fe8a7e3
brucemingxinliu <mxl592@case.edu>
2017-04-20 00:29:23 -0400

    Update inventory_control_service.cpp

--- src/inventory_control_service.cpp
+++ src/inventory_control_service.cpp
@@ -5,6 +5,7 @@
 #include <cwru_ariac/Part.h>
 #include <cwru_ariac/InventoryServiceMsg.h>
 #include "CameraEstimator.h"
+#include "AriacBase.h"
 #include <iostream>
 #include <string>
 #include <utility>
@@ -27,6 +28,10 @@ geometry_msgs::PoseStamped _xPose;
 geometry_msgs::PoseStamped _yPose;
 geometry_msgs::PoseStamped _x_Pose;
 geometry_msgs::PoseStamped _y_Pose;
+const string defaultPartsNames[totalPartsTypes] = {"piston_rod_part", "gear_part", "pulley_part", "gasket_part",
+                                                  "part1", "part2", "part3", "part4"};
+const double defaultPartsSizes[totalPartsTypes][2] = {{0.059,0.052}, {0.078425,0.078425}, {0.23392,0.23392}, {0.31442,0.15684},
+                                                     {0.3,0.1}, {0.06,0.015}, {0.13,0.07}, {0.09,0.06}};
 
 
 std::pair<string, double> part_names[8];
@@ -37,14 +42,15 @@ bool callback(cwru_ariac::InventoryServiceMsgRequest& request, cwru_ariac::Inven
 //for (bin[0]; bin[i]; i++){ // loop to check from first bin to the last
     ROS_INFO("callback activated");
     string part_name(request.part_name);
+   // string abcd = camera.nextPart.name;
     for(i = 0; i < 7; i++){
-        if(part_name.compare(part_names[i].first) == 1){
+        if(part_name.compare(defaultPartsName[i]) == 1){
             radius = part_names[i].second;
         }
 
     }
 
-    for (int j = 0; j < TotalNumOfParts; j++){ // loop to check each location on each bin
+   /* for (int j = 0; j < TotalNumOfParts; j++){ // loop to check each location on each bin
     	if(part.traceable = false && nextPart.traceable = true){
     		part_exists = true;
     		emptySpotCount = emptySpotCount + 1;
@@ -57,7 +63,7 @@ bool callback(cwru_ariac::InventoryServiceMsgRequest& request, cwru_ariac::Inven
     	}
 
     }
-
+*/
 
     for (i = 0; i <= emptySpotCount; ++i){ // collision check
     	/*if(PoseStamped_xPose(i) > bins[i].pose.pose.position.x + half_bin_size && PoseStamped_xPose(i) < part.pose.pose.position.x - half_bin_size){
@@ -69,7 +75,7 @@ bool callback(cwru_ariac::InventoryServiceMsgRequest& request, cwru_ariac::Inven
     			y = PoseStamped_yPose(i);
 
     		}*/
-            for( k = 0; k <= spot.size() ; k++){}
+            /*for( k = 0; k <= spot.size() ; k++){}
     		if(abs(_xPose(k) - _x_Pose(k)) < radius + (double)(spot(k)) && abs(_yPose(k) - _y_Pose(k)) < radius + (double)(spot(k))){ 
     			ROS_WARN("The current locations are: x = %d  y = %d", x , y);
                 x = _xPose.pose.position.x;
@@ -78,96 +84,55 @@ bool callback(cwru_ariac::InventoryServiceMsgRequest& request, cwru_ariac::Inven
                 response_pose.pose.position.x = x;
                 response_pose.pose.position.y = y;
                 reponse.pose.push_back(response_pose);
-    		}
+    		}*/
     	}
     }
-    }
-}
+    
+
     int main(int argc, char** argv){
         ros::init(argc, argv, "inventory_control_service");
         ros::NodeHandle nh;
         ros::ServiceServer service = nh.advertiseService("look_up_parts_space",callback); 
-        ros::Subscriber subscriber = nh.subscribe("/ariac/logical_camera_1", 10, callback);
+
+		CameraEstimator camera(nh, "/ariac/logical_camera_1");
+		//camera.ForceUpdate();
+
+		for (auto part : camera.inView) {
+	    ROS_INFO_STREAM("This is pose stamped of each part:\n" << part.pose); // also equal to outPose
+	    ROS_INFO_STREAM("This is pose of each part:\n" << part.pose.pose);
+	    ROS_INFO_STREAM("This is the name of each part:\n" << part.name);
+}
         ROS_INFO("Ready to look up parts.");
+        /*
         part_names[0].first = "piston_rod_part_1"; // Bin1.. 12
-	part_names[0].second = 0.06;
+		part_names[0].second = 0.06;
 
-	part_names[1].first = "Pulley_part_1"; //Bin2 .. 4
-	part_names[1].second = 0.12;
+		part_names[1].first = "Pulley_part_1"; //Bin2 .. 4
+		part_names[1].second = 0.12;
 
-	part_names[2].first = "Gear_part"; //Bin3..25
-	part_names[2].second = 0.03;
+		part_names[2].first = "Gear_part"; //Bin3..25
+		part_names[2].second = 0.03;
 
-	part_names[3].first = "gasket_part"; //Bin4 .. 4
-	part_names[3].second = 0.1;
+		part_names[3].first = "gasket_part"; //Bin4 .. 4
+		part_names[3].second = 0.1;
 
-	part_names[4].first = "Part_4"; // Bin5 .. 25
-	part_names[4].second = 0.03;
+		part_names[4].first = "Part_4"; // Bin5 .. 25
+		part_names[4].second = 0.03;
 
-	part_names[5].first = "Part_3"; // Bin6 .. 12
-	part_names[5].second = 0.06;
+		part_names[5].first = "Part_3"; // Bin6 .. 12
+		part_names[5].second = 0.06;
 
-	part_names[6].first = "piston_rod_part_2"; // Bin7.. 12
-	part_names[6].second = 0.06;
+		part_names[6].first = "piston_rod_part_2"; // Bin7.. 12
+		part_names[6].second = 0.06;
 
-	part_names[7].first = "Pulley_part_2"; //Bin2 .. 4
-	part_names[7].second = 0.12;
+		part_names[7].first = "Pulley_part_2"; //Bin2 .. 4
+		part_names[7].second = 0.12;
 //BinManager::BinManager(ros::NodeHandle nodeHandle){ // constructor of BinManager
 //BinManger.assignCamera(); // Call the logic camera
+*/
 
-}
-for (int i = 0; i < totalBins; ++i) {
-        bins.push_back(defaultBin);
 
-}
-
-        bins[0].name = "Bin4";
-        bins[0].priority = 6.0;
-        bins[0].pose.pose.position.x = -1.000000;
-        bins[0].pose.pose.position.y = 0.995000;
-        bins[0].pose.pose.position.z = 0.0;
-
-        bins[1].name = "Bin8";
-        bins[1].priority = 10.0;
-        bins[1].pose.pose.position.x = -0.300000;
-        bins[1].pose.pose.position.y = 0.995000;
-        bins[1].pose.pose.position.z = 0.0;
-
-        bins[2].name = "Bin3";
-        bins[2].priority = 4.0;
-        bins[2].pose.pose.position.x = -1.000000;
-        bins[2].pose.pose.position.y = 0.230000;
-        bins[2].pose.pose.position.z = 0.0;
-
-        bins[3].name = "Bin7";
-        bins[3].priority = 8.0;
-        bins[3].pose.pose.position.x = -0.300000;
-        bins[3].pose.pose.position.y = 0.230000;
-        bins[3].pose.pose.position.z = 0.0;
-   
-        bins[4].name = "Bin2";
-        bins[4].priority = 3.0;
-        bins[4].pose.pose.position.x = -1.000000;
-        bins[4].pose.pose.position.y = -0.535000;
-        bins[4].pose.pose.position.z = 0.0;
-
-        bins[5].name = "Bin6";
-        bins[5].priority = 7.0;
-        bins[5].pose.pose.position.x = -0.300000;
-        bins[5].pose.pose.position.y = -0.535000;
-        bins[5].pose.pose.position.z = 0.0;
-
-        bins[6].name = "Bin1";
-        bins[6].priority = 5.0;
-        bins[6].pose.pose.position.x = -1.000000;
-        bins[6].pose.pose.position.y = -1.330000;
-        bins[6].pose.pose.position.z = 0.0;
- 
-        bins[7].name = "Bin5";
-        bins[7].priority = 9.0;
-        bins[7].pose.pose.position.x = -0.300000;
-        bins[7].pose.pose.position.y = -1.330000;
-        bins[7].pose.pose.position.z = 0.0;
+        
 
         ros::spin();
         return 0;
