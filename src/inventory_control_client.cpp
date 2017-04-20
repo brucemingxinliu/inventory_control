d2a6ed036ddb5ccf74f6aee3fb3685be0f3256c7
brucemingxinliu <mxl592@case.edu>
2017-04-20 00:56:44 -0400

    Update inventory_control_client.cpp

--- src/inventory_control_client.cpp
+++ src/inventory_control_client.cpp
@@ -19,20 +19,27 @@ int main(int argc, char **argv){
 	cwru_ariac::InventoryServiceMsg srv;
 	string part_name;
 
+    while (!client.exists()) {
+      ROS_INFO("waiting for service...");
+      ros::Duration(1.0).sleep();
+    }
+	ROS_INFO("connected to service");
+
 	while (ros::ok()){
-            for(geometry_msgs::PoseStamped pose : srv.response.poses)
-            {
+         //   for(geometry_msgs::PoseStamped pose : srv.response.poses)
+        //    {
 		cout<<endl;
 		cout<<"What's the part's name that has the highest priority? ";
 		cin>>part_name;
 		srv.request.part_name = part_name;// the name of the part is sending to the service to check spacial avaliablity
+	
 		if(client.call(srv)){
-			cout << "the x coordinate is " << pose.pose.position.x << "and the y coordinate is" << pose.pose.position.y << endl;
+		//	cout << "the x coordinate is " << pose.pose.position.x << "and the y coordinate is" << pose.pose.position.y << endl;
 
 
 
 		}
-             }
+         //    }
 	}
 
 
