651ac19181e415725e4b267cbc014fd781c88a85
brucemingxinliu <mxl592@case.edu>
2017-04-22 13:02:09 -0400

    Update inventory_control_service.cpp

--- src/inventory_control_service.cpp
+++ src/inventory_control_service.cpp
@@ -1,7 +1,7 @@
-//create by Bruce Mingxin Liu on Apirl 15th 2017, 4:47 pm
+/* create by Bruce Mingxin Liu on Apirl 15th 2017, 4:47 pm */
 
 #include <ros/ros.h>
-//#include "BinManger.h"
+/* #include "BinManger.h" */
 #include <cwru_ariac/Part.h>
 #include <cwru_ariac/InventoryServiceMsg.h>
 #include "CameraEstimator.h"
@@ -12,112 +12,127 @@
 using namespace std;
 
 
-bool part_exists = false; // To check if the number of parts on the bin is the same as the proposed number
-int TotalNumOfParts = 103; // the total number of parts or locations to put the parts.
-int i;
-int emptySpotCount;
-double half_bin_size = 0.3; // what is the bin size...
-double r; // radius of the priority part..
-double x;
-double y;
-double radius;
-int k;
+bool        part_exists = false;        /* To check if the number of parts on the bin is the same as the proposed number */
+int     TotalNumOfParts = 103;          /* the total number of parts or locations to put the parts. */
+int     i;
+int     emptySpotCount;
+double      half_bin_size = 0.3;            /* what is the bin size... */
+double      r;                              /* radius of the priority part.. */
+double      x;
+double      y;
+double      radius;
+int     k;
+CameraEstimator *camera_ptr;
 
-
-std::vector<double> part_size;
-geometry_msgs::PoseStamped _xPose;
-geometry_msgs::PoseStamped _yPose;
-geometry_msgs::PoseStamped _x_Pose;
-geometry_msgs::PoseStamped _y_Pose;
-geometry_msgs::PoseStamped part;
-const string defaultPartsNames[totalPartsTypes] = {"piston_rod_part", "gear_part", "pulley_part", "gasket_part",
-                                                  "part1", "part2", "part3", "part4"};
-const double defaultPartsSizes[totalPartsTypes][2] = {{0.059,0.052}, {0.078425,0.078425}, {0.23392,0.23392}, {0.31442,0.15684},
-                                                     {0.3,0.1}, {0.06,0.015}, {0.13,0.07}, {0.09,0.06}};
+std::vector<double>     part_size;
+geometry_msgs::PoseStamped  _xPose;
+geometry_msgs::PoseStamped  _yPose;
+geometry_msgs::PoseStamped  _x_Pose;
+geometry_msgs::PoseStamped  _y_Pose;
+geometry_msgs::PoseStamped  part;
+const string            defaultPartsNames[totalPartsTypes] = { "piston_rod_part", "gear_part", "pulley_part", "gasket_part",
+                                       "part1",                  "part2",     "part3",   "part4" };
+const double            defaultPartsSizes[totalPartsTypes][2] = { { 0.059, 0.052 }, { 0.078425, 0.078425 }, { 0.23392, 0.23392 }, { 0.31442, 0.15684 },
+                                      { 0.3,   0.1   }, { 0.06, 0.015    }, { 0.13,    0.07    }, { 0.09,    0.06    } };
 
 
 std::pair<string, double> part_names[8];
 
 
+bool callback( cwru_ariac::InventoryServiceMsgRequest & request, cwru_ariac::InventoryServiceMsgResponse & response )
+{
+    /* for (bin[0]; bin[i]; i++){ // loop to check from first bin to the last */
+    ROS_INFO( "callback activated" );
+    string part_name( request.part_name );
+    /* string abcd = camera.nextPart.name; */
 
 
-
-bool callback(cwru_ariac::InventoryServiceMsgRequest& request, cwru_ariac::InventoryServiceMsgResponse& response){
-//for (bin[0]; bin[i]; i++){ // loop to check from first bin to the last
-    ROS_INFO("callback activated");
-    string part_name(request.part_name);
-   // string abcd = camera.nextPart.name;
-  
-    
-    for(i = 0; i < 7; i++){
-        if(part_name.compare(defaultPartsName[i]) == 1){
-            
-            x = defaultPartsSizes[i][1];
-            y = defaultPartsSizes[i][2];
+    for ( i = 0; i < 7; i++ )
+    {
+        if ( part_name.compare( defaultPartsName[i] ) == 1 )
+        {
+            x   = defaultPartsSizes[i][1];
+            y   = defaultPartsSizes[i][2];
         }
-
     }
 
-   /* for (int j = 0; j < TotalNumOfParts; j++){ // loop to check each location on each bin
-    	if(part.traceable = false && nextPart.traceable = true){
-    		part_exists = true;
-    		emptySpotCount = emptySpotCount + 1;
-    		part.pose.pose.position.x.push_back(_xPose);
-    		part.pose.pose.position.y.push_back(_yPose);
-    		next.Part.linear.x.push_back(_x_Pose); 
-            next.Part.linear.y.push_back(_y_Pose);
-            spot.push_back(j); // saving all radius of the locations
-
-    	}
-
-    }
-*/
-
-    for (i = 0; i <= emptySpotCount; ++i){ // collision check
-    	/*if(PoseStamped_xPose(i) > bins[i].pose.pose.position.x + half_bin_size && PoseStamped_xPose(i) < part.pose.pose.position.x - half_bin_size){
-
-    		if (PoseStamped_yPose(i) > part.pose.pose.position.y + half_bin_size && PoseStamped_yPose(i) < part.pose.pose.position.y - half_bin_size){
-    			return bins[i].name; //checking what are the parts should be on this specific bin then check the spatial avaliability
-    			int ptr = i;
-    			x = PoseStamped_xPose(i);
-    			y = PoseStamped_yPose(i);
-
-    		}*/
-            /*for( k = 0; k <= spot.size() ; k++){}
-    		if(abs(_xPose(k) - _x_Pose(k)) < radius + (double)(spot(k)) && abs(_yPose(k) - _y_Pose(k)) < radius + (double)(spot(k))){ 
-    			ROS_WARN("The current locations are: x = %d  y = %d", x , y);
-                x = _xPose.pose.position.x;
-                y = _yPose.pose.position.y;
-                geometry_msgs::PoseStamped response_pose;
-                response_pose.pose.position.x = x;
-                response_pose.pose.position.y = y;
-                reponse.pose.push_back(response_pose);
-    		}*/
-    	}
-    }
-    
-
-    int main(int argc, char** argv){
-        ros::init(argc, argv, "inventory_control_service");
-        ros::NodeHandle nh;
-        ros::ServiceServer service = nh.advertiseService("look_up_parts_space",callback); 
-
-		CameraEstimator camera(nh, "/ariac/logical_camera_1");
-    		//camera.ForceUpdate();
+    camera_ptr->ForceUpdate();
 
 
-         ROS_INFO("THIS IS A TEST");
-
-		for (auto part : camera.inView) {
-	    ROS_INFO_STREAM("This is pose stamped of each part:\n" << part.pose); // also equal to outPose
-	    ROS_INFO_STREAM("This is pose of each part:\n" << part.pose.pose);
-	    ROS_INFO_STREAM("This is the name of each part:\n" << part.name);
-    }
-        ROS_INFO("Ready to look up parts.");
-
-
-        ros::spin();
-        return 0;
-
+    // for ( auto part : camera_ptr->inView )
+    // {
+    //     ROS_INFO_STREAM( "This is pose stamped of each part:\n" << part.pose ); /* also equal to outPose */
+    //     ROS_INFO_STREAM( "This is pose of each part:\n" << part.pose.pose );
+    //     ROS_INFO_STREAM( "This is the name of each part:\n" << part.name );
+    // }
+    auto part_list = findPart(camera_ptr->inView, part_name);
+    for (auto part : part_list) {
+        response.poses.push_back(part.pose);
     }
 
+    /* for (int j = 0; j < TotalNumOfParts; j++){ // loop to check each location on each bin
+     *   if(part.traceable = false && nextPart.traceable = true){
+     *       part_exists = true;
+     *       emptySpotCount = emptySpotCount + 1;
+     *       part.pose.pose.position.x.push_back(_xPose);
+     *       part.pose.pose.position.y.push_back(_yPose);
+     *       next.Part.linear.x.push_back(_x_Pose);
+     *       next.Part.linear.y.push_back(_y_Pose);
+     *       spot.push_back(j); // saving all radius of the locations
+     *
+     *   }
+     *
+     * }
+     */
+
+    for ( i = 0; i <= emptySpotCount; ++i ) /* collision check */
+    { /*if(PoseStamped_xPose(i) > bins[i].pose.pose.position.x + half_bin_size && PoseStamped_xPose(i) < part.pose.pose.position.x - half_bin_size){
+         *
+         * if (PoseStamped_yPose(i) > part.pose.pose.position.y + half_bin_size && PoseStamped_yPose(i) < part.pose.pose.position.y - half_bin_size){
+         * return bins[i].name; //checking what are the parts should be on this specific bin then check the spatial avaliability
+         * int ptr = i;
+         * x = PoseStamped_xPose(i);
+         * y = PoseStamped_yPose(i);
+         *
+         * }*/
+        /*for( k = 0; k <= spot.size() ; k++){}
+         * if(abs(_xPose(k) - _x_Pose(k)) < radius + (double)(spot(k)) && abs(_yPose(k) - _y_Pose(k)) < radius + (double)(spot(k))){
+         *  ROS_WARN("The current locations are: x = %d  y = %d", x , y);
+         *  x = _xPose.pose.position.x;
+         *  y = _yPose.pose.position.y;
+         *  geometry_msgs::PoseStamped response_pose;
+         *  response_pose.pose.position.x = x;
+         *  response_pose.pose.position.y = y;
+         *  reponse.pose.push_back(response_pose);
+         * }*/
+    }
+}
+
+
+int main( int argc, char** argv )
+{
+    ros::init( argc, argv, "inventory_control_service" );
+    ros::NodeHandle     nh;
+    ros::ServiceServer  service = nh.advertiseService( "look_up_parts_space", callback );
+
+    CameraEstimator camera( nh, "/ariac/logical_camera_1" );
+    camera_ptr = &camera;
+    camera.ForceUpdate();
+
+
+    ROS_INFO( "THIS IS A TEST" );
+    // for ( auto part : camera.inView )
+    // {
+    //     ROS_INFO_STREAM( "This is pose stamped of each part:\n" << part.pose ); /* also equal to outPose */
+    //     ROS_INFO_STREAM( "This is pose of each part:\n" << part.pose.pose );
+    //     ROS_INFO_STREAM( "This is the name of each part:\n" << part.name );
+    //     part.pose.pose.position.x;
+    // }
+
+
+    ROS_INFO( "Ready to look up parts." );
+
+
+    ros::spin();
+    return(0);
+}
